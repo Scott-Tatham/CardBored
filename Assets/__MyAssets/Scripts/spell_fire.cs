af291c0ef50cd4f59f013c01c5cbfb74e8108ed0
SamGiumelli <youtube_ninjatendo@hotmail.com>
2016-08-11 11:09:22 +1000

    [Created Branch]
    -added conventions to project
    -created spell scripts
    -edited folder structure

---
+++ Assets/__MyAssets/Scripts/spell_fire.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spell_fire : MonoBehaviour
+{
+	#region Vars
+
+	#endregion
+	#region Stock/Inherent
+	void Start()
+	{
+
+	}
+	void Update()
+	{
+
+	}
+	#endregion
+	#region Custom
+
+	#endregion
+}
