3dd10569bf45326e2241e1b05ecb642304a5b996
Scott-Tatham <scottytatham@gmail.com>
2016-09-01 13:25:04 +1000

    Defence Building Demo
    A version used to demonstate our current defence building.

--- Assets/Scripts/DefenceBuilding/BlockFunctionality/Slide.cs
+++ Assets/Scripts/DefenceBuilding/BlockFunctionality/Slide.cs
@@ -41,27 +41,30 @@ public class Slide : BaseBlock
         {
             if (bb[Mathf.RoundToInt(GetBZSpace().GetBZSpace().x + Vector3.forward.x), Mathf.RoundToInt(GetBZSpace().GetBZSpace().y + Vector3.forward.y), Mathf.RoundToInt(GetBZSpace().GetBZSpace().z + Vector3.forward.z)] != null && bb[Mathf.RoundToInt(GetBZSpace().GetBZSpace().x + Vector3.forward.x), Mathf.RoundToInt(GetBZSpace().GetBZSpace().y + Vector3.forward.y), Mathf.RoundToInt(GetBZSpace().GetBZSpace().z + Vector3.forward.z)].tag != "StaticBlock" && bb[Mathf.RoundToInt(GetBZSpace().GetBZSpace().x + Vector3.forward.x), Mathf.RoundToInt(GetBZSpace().GetBZSpace().y + Vector3.forward.y), Mathf.RoundToInt(GetBZSpace().GetBZSpace().z + Vector3.forward.z)].tag != "SlideBlock")
             {
-                Debug.Log(bb[Mathf.RoundToInt(GetBZSpace().GetBZSpace().x + Vector3.forward.x), Mathf.RoundToInt(GetBZSpace().GetBZSpace().y + Vector3.forward.y), Mathf.RoundToInt(GetBZSpace().GetBZSpace().z + Vector3.forward.z)].tag);
                 BaseBlock b = bb[Mathf.RoundToInt(GetBZSpace().GetBZSpace().x + Vector3.forward.x), Mathf.RoundToInt(GetBZSpace().GetBZSpace().y + Vector3.forward.y), Mathf.RoundToInt(GetBZSpace().GetBZSpace().z + Vector3.forward.z)];
 
-                if (b.transform.position == target[0])
+                if (b.transform.position.x >= target[0].x && b.transform.position.y >= target[0].y && b.transform.position.z >= target[0].z)
                 {
                     switch (dir[0])
                     {
                         case Direction.RIGHT:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.right.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.right.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.right.z));
+                            bz.MoveBlock(b, Vector3.right);
                             break;
 
                         case Direction.LEFT:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.left.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.left.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.left.z));
+                            bz.MoveBlock(b, Vector3.left);
                             break;
 
                         case Direction.UP:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.up.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.up.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.up.z));
+                            bz.MoveBlock(b, Vector3.up);
                             break;
 
                         case Direction.DOWN:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.down.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.down.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.down.z));
+                            bz.MoveBlock(b, Vector3.down);
                             break;
                     }
 
@@ -92,6 +95,8 @@ public class Slide : BaseBlock
                     init[0] = false;
                 }
 
+                Debug.Log(target[0]);
+
                 if (InRange(target[0]))
                 {
                     b.transform.position = Vector3.MoveTowards(b.transform.position, target[0], 1 * Time.deltaTime);
@@ -105,24 +110,28 @@ public class Slide : BaseBlock
             {
                 BaseBlock b = bb[Mathf.RoundToInt(GetBZSpace().GetBZSpace().x + Vector3.back.x), Mathf.RoundToInt(GetBZSpace().GetBZSpace().y + Vector3.back.y), Mathf.RoundToInt(GetBZSpace().GetBZSpace().z + Vector3.back.z)];
 
-                if (b.transform.position == target[1])
+                if (b.transform.position.x >= target[1].x && b.transform.position.y >= target[1].y && b.transform.position.z >= target[1].z)
                 {
                     switch (dir[1])
                     {
                         case Direction.RIGHT:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.right.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.right.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.right.z));
+                            bz.MoveBlock(b, Vector3.right);
                             break;
 
                         case Direction.LEFT:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.left.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.left.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.left.z));
+                            bz.MoveBlock(b, Vector3.left);
                             break;
 
                         case Direction.UP:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.up.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.up.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.up.z));
+                            bz.MoveBlock(b, Vector3.up);
                             break;
 
                         case Direction.DOWN:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.down.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.down.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.down.z));
+                            bz.MoveBlock(b, Vector3.down);
                             break;
                     }
 
@@ -153,6 +162,8 @@ public class Slide : BaseBlock
                     init[1] = false;
                 }
 
+                Debug.Log(target[1]);
+
                 if (InRange(target[1]))
                 {
                     b.transform.position = Vector3.MoveTowards(b.transform.position, target[1], 1 * Time.deltaTime);
@@ -166,24 +177,28 @@ public class Slide : BaseBlock
             {
                 BaseBlock b = bb[Mathf.RoundToInt(GetBZSpace().GetBZSpace().x + Vector3.right.x), Mathf.RoundToInt(GetBZSpace().GetBZSpace().y + Vector3.right.y), Mathf.RoundToInt(GetBZSpace().GetBZSpace().z + Vector3.right.z)];
 
-                if (b.transform.position == target[2])
+                if (b.transform.position.x >= target[2].x && b.transform.position.y >= target[2].y && b.transform.position.z >= target[2].z)
                 {
                     switch (dir[2])
                     {
                         case Direction.FORWARD:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.forward.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.forward.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.forward.z));
+                            bz.MoveBlock(b, Vector3.forward);
                             break;
 
                         case Direction.BACK:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.back.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.back.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.back.z));
+                            bz.MoveBlock(b, Vector3.back);
                             break;
 
                         case Direction.UP:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.up.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.up.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.up.z));
+                            bz.MoveBlock(b, Vector3.up);
                             break;
 
                         case Direction.DOWN:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.down.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.down.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.down.z));
+                            bz.MoveBlock(b, Vector3.down);
                             break;
                     }
 
@@ -214,6 +229,8 @@ public class Slide : BaseBlock
                     init[2] = false;
                 }
 
+                Debug.Log(target[2]);
+
                 if (InRange(target[2]))
                 {
                     b.transform.position = Vector3.MoveTowards(b.transform.position, target[2], 1 * Time.deltaTime);
@@ -227,24 +244,28 @@ public class Slide : BaseBlock
             {
                 BaseBlock b = bb[Mathf.RoundToInt(GetBZSpace().GetBZSpace().x + Vector3.left.x), Mathf.RoundToInt(GetBZSpace().GetBZSpace().y + Vector3.left.y), Mathf.RoundToInt(GetBZSpace().GetBZSpace().z + Vector3.left.z)];
 
-                if (b.transform.position == target[3])
+                if (b.transform.position.x >= target[3].x && b.transform.position.y >= target[3].y && b.transform.position.z >= target[3].z)
                 {
                     switch (dir[3])
                     {
                         case Direction.FORWARD:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.forward.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.forward.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.forward.z));
+                            bz.MoveBlock(b, Vector3.forward);
                             break;
 
                         case Direction.BACK:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.back.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.back.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.back.z));
+                            bz.MoveBlock(b, Vector3.back);
                             break;
 
                         case Direction.UP:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.up.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.up.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.up.z));
+                            bz.MoveBlock(b, Vector3.up);
                             break;
 
                         case Direction.DOWN:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.down.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.down.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.down.z));
+                            bz.MoveBlock(b, Vector3.down);
                             break;
                     }
 
@@ -275,6 +296,8 @@ public class Slide : BaseBlock
                     init[3] = false;
                 }
 
+                Debug.Log(target[3]);
+
                 if (InRange(target[3]))
                 {
                     b.transform.position = Vector3.MoveTowards(b.transform.position, target[3], 1 * Time.deltaTime);
@@ -288,24 +311,28 @@ public class Slide : BaseBlock
             {
                 BaseBlock b = bb[Mathf.RoundToInt(GetBZSpace().GetBZSpace().x + Vector3.up.x), Mathf.RoundToInt(GetBZSpace().GetBZSpace().y + Vector3.up.y), Mathf.RoundToInt(GetBZSpace().GetBZSpace().z + Vector3.up.z)];
 
-                if (b.transform.position == target[4])
+                if (b.transform.position.x >= target[4].x && b.transform.position.y >= target[4].y && b.transform.position.z >= target[4].z)
                 {
                     switch (dir[4])
                     {
                         case Direction.FORWARD:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.forward.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.forward.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.forward.z));
+                            bz.MoveBlock(b, Vector3.forward);
                             break;
 
                         case Direction.BACK:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.back.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.back.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.back.z));
+                            bz.MoveBlock(b, Vector3.back);
                             break;
 
                         case Direction.RIGHT:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.right.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.right.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.right.z));
+                            bz.MoveBlock(b, Vector3.right);
                             break;
 
                         case Direction.LEFT:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.left.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.left.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.left.z));
+                            bz.MoveBlock(b, Vector3.left);
                             break;
                     }
 
@@ -336,6 +363,8 @@ public class Slide : BaseBlock
                     init[4] = false;
                 }
 
+                Debug.Log(target[4]);
+
                 if (InRange(target[4]))
                 {
                     b.transform.position = Vector3.MoveTowards(b.transform.position, target[4], 1 * Time.deltaTime);
@@ -349,24 +378,28 @@ public class Slide : BaseBlock
             {
                 BaseBlock b = bb[Mathf.RoundToInt(GetBZSpace().GetBZSpace().x + Vector3.down.x), Mathf.RoundToInt(GetBZSpace().GetBZSpace().y + Vector3.down.y), Mathf.RoundToInt(GetBZSpace().GetBZSpace().z + Vector3.down.z)];
 
-                if (b.transform.position == target[5])
+                if (b.transform.position.x >= target[5].x && b.transform.position.y >= target[5].y && b.transform.position.z >= target[5].z)
                 {
                     switch (dir[5])
                     {
                         case Direction.FORWARD:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.forward.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.forward.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.forward.z));
+                            bz.MoveBlock(b, Vector3.forward);
                             break;
 
                         case Direction.BACK:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.back.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.back.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.back.z));
+                            bz.MoveBlock(b, Vector3.back);
                             break;
 
                         case Direction.RIGHT:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.right.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.right.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.right.z));
+                            bz.MoveBlock(b, Vector3.right);
                             break;
 
                         case Direction.LEFT:
                             b.SetBZSpace(Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().x + Vector3.left.x), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().y + Vector3.left.y), Mathf.RoundToInt(b.GetBZSpace().GetBZSpace().z + Vector3.left.z));
+                            bz.MoveBlock(b, Vector3.left);
 
                             break;
                     }
@@ -398,6 +431,8 @@ public class Slide : BaseBlock
 
                     init[5] = false;
                 }
+
+                Debug.Log(target[5]);
 
                 if (InRange(target[5]))
                 {
