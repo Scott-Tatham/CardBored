275fd332a2ce022772e9f4dd503dd1f0e2846779
Scott-Tatham <scottytatham@gmail.com>
2016-09-08 12:06:03 +1000

    Revert "Revert "More Of The Same Defence Building Stuff""
    This reverts commit 4fea1888da138fe5e8db874e4094502a69495538.

---
+++ Assets/Scripts/DefenceBuilding/BlockFunctionality/Rotate.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : PowerNode
+{
+<<<<<<< HEAD
+    [SerializeField]
+    float turnSpeed;
+    bool[] dir = new bool[6] { true, true, true, true, true, true };
+=======
+    private bool[] dir = new bool[6] { true, true, true, true, true, true };
+>>>>>>> parent of 7df8639... More Of The Same Defence Building Stuff
+
+    protected override void Update()
+    {
+        base.Update();
+
+        RotateF();
+    }
+
+    void RotateF()
+    {
+        if (et.GetCanDo())
+        {
+            SetFace();
+
+
+            for (int i = 0; i < face.Length; i++)
+            {
+                if (isActive[i] && isPowered)
+                {
+                    if (face[i] != null && face[i].GetComponent<PowerNode>() == null && face[i].transform.parent == null && face[i].tag != "StaticBlock")
+                    {
+                        if (face[i].GetCanStart())
+                        {
+                            turnSpeed = dir[i] == true ? turnSpeed * 1 : turnSpeed * -1;
+
+                            face[i].transform.RotateAround(transform.position, (face[i].transform.position - transform.position).normalized, turnSpeed);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
